8d5c8fb6d0a9154f8c46bdd8de5bb10ca15f2307
myxzzz <1806269024@qq.com>
2026-02-22 21:48:00 +0800

    初始提交：创建学习仓库

---
+++ test.py
@@ -0,0 +1,13 @@
+# 测试文件 - 我的第一个 Python 脚本
+
+# 这是一个简单的测试
+print("Hello Git!")
+
+# 学习 Pandas
+import pandas as pd
+
+# 创建一个简单的数据框
+data = {"品牌": ["联想", "华为", "苹果"], "价格": [5000, 6000, 9000]}
+
+df = pd.DataFrame(data)
+print(df)
